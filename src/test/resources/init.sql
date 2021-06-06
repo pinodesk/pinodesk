8b8bda9b344d5d27f6e4f7e53dcf8fc1fef6841d
Muhammad <muhammadkholidb@gmail.com>
2021-06-06 13:56:46 +0700

    update t_product_category dataset

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -60,10 +60,6 @@ create table if not exists t_language (
 	index idx_t_language__code (code)
 );
 
-insert into t_language (id, created_at, updated_at, deleted_at, code, name) values
-(null, current_timestamp, current_timestamp, null, 'eng', 'English'),
-(null, current_timestamp, current_timestamp, null, 'ind', 'Bahasa Indonesia');
-
 create table if not exists t_product_category (
 	id bigint not null auto_increment,
 	created_at timestamp not null default current_timestamp,
