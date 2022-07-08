9d9068b1886ebee1b5c9283f5c54a5a968abfee7
Muhammad <muhammadkholidb@gmail.com>
2022-07-08 19:50:35 +0700

    create script db-recovery.sh

---
+++ db-recovery.sh
@@ -0,0 +1,17 @@
+cd ~/.pinus/db
+
+echo "Create recovery SQL script"
+$(which java) -cp ~/.m2/repository/com/h2database/h2/2.1.212/h2-2.1.212.jar org.h2.tools.Recover
+
+echo "Create temp folder"
+mkdir -p ~/.pinus/db/temp
+
+echo "Move corrupted db to temp folder"
+mv pinusdb.mv.db pinusdb.trace.db ~/.pinus/db/temp
+
+echo "Execute recovery SQL script"
+java -cp ~/.m2/repository/com/h2database/h2/2.1.212/h2-2.1.212.jar org.h2.tools.RunScript \
+-url "jdbc:h2:~/.pinus/db/pinusdb;MODE=MySQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_ON_EXIT=FALSE" \
+-script pinusdb.h2.sql
+
+echo "Database recovery done"
