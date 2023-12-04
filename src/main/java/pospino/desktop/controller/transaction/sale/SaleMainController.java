e900c09584966e48feac7ab781c7ef7dc5b97d4f
Muhammad Kholid B <muhammadkholidb@gmail.com>
2023-12-04 20:37:23 +0700

    fix: enable opening multiple cashier pages

--- src/main/java/pospino/desktop/controller/transaction/sale/SaleMainController.java
+++ src/main/java/pospino/desktop/controller/transaction/sale/SaleMainController.java
@@ -130,13 +130,11 @@ public class SaleMainController extends BaseController {
 
     @FXML
     void onActionBtnCashier(ActionEvent event) {
-        btnCashier.setDisable(true);
-        StageUtils.modal(Page.TRANSACTION_SALE_CASHIER_MAIN, true, we -> {
+        StageUtils.open(Page.TRANSACTION_SALE_CASHIER_MAIN, true, we -> {
             // Remove the last data from the stack and ignore (if not used) to avoid such
             // this issue https://gitlab.com/pospino/pospino-desktop/-/issues/52
             getPageData();
             searchSales();
-            btnCashier.setDisable(false);
         });
     }
 
